17e6627a26276fffc5245ca252b19945a3f591af
HeiReshKienai <kienadu123@gmail.com>
2023-12-24 04:55:26 +0700

    1

---
+++ QLHOCSINHCAP3/UI/uc_QuanLyTaiKhoanHocSinh.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace QLHOCSINHCAP3.UI {
+    partial class uc_QuanLyTaiKhoanHocSinh {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dgvChuaTK = new System.Windows.Forms.DataGridView();
+            this.dgvCoTK = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtMaHS = new System.Windows.Forms.TextBox();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChuaTK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCoTK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvChuaTK
+            // 
+            this.dgvChuaTK.AllowUserToAddRows = false;
+            this.dgvChuaTK.AllowUserToDeleteRows = false;
+            this.dgvChuaTK.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChuaTK.Location = new System.Drawing.Point(586, 213);
+            this.dgvChuaTK.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvChuaTK.Name = "dgvChuaTK";
+            this.dgvChuaTK.ReadOnly = true;
+            this.dgvChuaTK.RowHeadersWidth = 51;
+            this.dgvChuaTK.RowTemplate.Height = 24;
+            this.dgvChuaTK.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvChuaTK.Size = new System.Drawing.Size(439, 373);
+            this.dgvChuaTK.TabIndex = 63;
+            // 
+            // dgvCoTK
+            // 
+            this.dgvCoTK.AllowUserToAddRows = false;
+            this.dgvCoTK.AllowUserToDeleteRows = false;
+            this.dgvCoTK.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCoTK.Location = new System.Drawing.Point(66, 213);
+            this.dgvCoTK.Name = "dgvCoTK";
+            this.dgvCoTK.ReadOnly = true;
+            this.dgvCoTK.RowHeadersWidth = 51;
+            this.dgvCoTK.RowTemplate.Height = 24;
+            this.dgvCoTK.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCoTK.Size = new System.Drawing.Size(466, 373);
+            this.dgvCoTK.TabIndex = 59;
+            this.dgvCoTK.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCoTK_CellClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 24);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "Email ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(688, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 24);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Học Sinh Chưa Có Tài Khoản";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 24);
+            this.label4.TabIndex = 57;
+            this.label4.Text = "Mã Học Sinh";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 24);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Mật Khẩu ";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(213, 148);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(255, 35);
+            this.txtEmail.TabIndex = 52;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhau.Location = new System.Drawing.Point(213, 101);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(255, 35);
+            this.txtMatKhau.TabIndex = 53;
+            // 
+            // txtMaHS
+            // 
+            this.txtMaHS.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaHS.Location = new System.Drawing.Point(213, 49);
+            this.txtMaHS.Name = "txtMaHS";
+            this.txtMaHS.Size = new System.Drawing.Size(255, 35);
+            this.txtMaHS.TabIndex = 54;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(500, 147);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(103, 38);
+            this.btnXoa.TabIndex = 60;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(500, 96);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(103, 39);
+            this.btnSua.TabIndex = 61;
+            this.btnSua.Text = "Sửa ";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(500, 49);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(103, 41);
+            this.btnThem.TabIndex = 62;
+            this.btnThem.Text = "Thêm ";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // uc_QuanLyTaiKhoanHocSinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvChuaTK);
+            this.Controls.Add(this.dgvCoTK);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtMaHS);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Name = "uc_QuanLyTaiKhoanHocSinh";
+            this.Size = new System.Drawing.Size(1087, 629);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChuaTK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCoTK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvChuaTK;
+        private System.Windows.Forms.DataGridView dgvCoTK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtMaHS;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+    }
+}
